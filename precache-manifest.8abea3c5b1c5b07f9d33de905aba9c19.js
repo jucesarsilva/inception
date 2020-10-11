9fe8fb73ebb27b279cfa79c4e812c8469afc18df
jucesarsilva <julio.silva.somos@gmail.com>
2020-10-11 14:21:50 -0300

    Updates

---
+++ precache-manifest.8abea3c5b1c5b07f9d33de905aba9c19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5430c2a9966e0e50f0d4f318dd28f848",
+    "url": "/inception/index.html"
+  },
+  {
+    "revision": "60a5b4b2704176f0a861",
+    "url": "/inception/static/css/main.5b8148ff.chunk.css"
+  },
+  {
+    "revision": "7589545aaff5a722c0ea",
+    "url": "/inception/static/js/2.be1ba3be.chunk.js"
+  },
+  {
+    "revision": "b56bbc56f1557f7b0e0f5bc00447c3da",
+    "url": "/inception/static/js/2.be1ba3be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a5b4b2704176f0a861",
+    "url": "/inception/static/js/main.49729650.chunk.js"
+  },
+  {
+    "revision": "99a094a56fca0b635de2",
+    "url": "/inception/static/js/runtime-main.5dd4c834.js"
+  },
+  {
+    "revision": "cd56dee56ab2ac365b148a838ee71f32",
+    "url": "/inception/static/media/logo.cd56dee5.svg"
+  }
+]);
